f59fbd7b32f36cbe5fc3d369a9a17e55eafd955e
pan <pgu7@wisc.edu>
2021-11-08 10:41:36 -0600

    add profile page

---
+++ app/src/main/java/com/example/sayyes/ProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.sayyes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private NavigationBarView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        bottomNavigationView = findViewById(R.id.main_menu);
+        bottomNavigationView.setOnItemSelectedListener(bottomNavFunction);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+    }
+
+    private NavigationBarView.OnItemSelectedListener bottomNavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.add:
+                    fragment = new HomeFragment(); // TODO
+                    break;
+                case R.id.person:
+                    fragment = new HomeFragment(); // TODO
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
+
+}
