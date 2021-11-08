e82b940969c2a731ecee514e6e4780f4093f4691
YukeWu <wuyuke0406@gmail.com>
2021-11-08 00:22:31 -0600

    Part of viewing a post complete. Next step: make it scrollable.

--- app/src/main/java/com/example/sayyes/SinglePostView.java
+++ app/src/main/java/com/example/sayyes/SinglePostView.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +16,8 @@ public class SinglePostView extends AppCompatActivity {
     ImageView like_button = (ImageView) findViewById(R.id.likeButton);
     TextView like_count = (TextView) findViewById(R.id.likeCount);
     TextView post_author = (TextView) findViewById(R.id.authorPost);
+    TextView tags = (TextView) findViewById(R.id.tags_of_post);
+    TextView location = (TextView) findViewById(R.id.post_location);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,8 @@ public class SinglePostView extends AppCompatActivity {
         title.setText("This is a sample title");
         like_count.setText("1111");
         post_author.setText("author of the post");
+        tags.setText("#sample_tag1");
+        location.setText("777 University Avenue, Madison, WI, USA");
     }
 
     public void likeFunc(View view) {
