1b3d80851db578aee1e96593aef77860d470f71b
whitney.huang <whuang258@wisc.edu>
2021-12-04 23:36:33 -0600

    Added SharedPreferences for login

--- app/src/main/java/com/example/sayyes/LoginActivity.java
+++ app/src/main/java/com/example/sayyes/LoginActivity.java
@@ -2,16 +2,34 @@ package com.example.sayyes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        String emailKey = "email";
+        String passwordKey = "password";
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.sayyes", Context.MODE_PRIVATE);
+
+        if (!sharedPreferences.getString(emailKey, "").equals("") && !sharedPreferences.getString(passwordKey, "").equals("")){
+            String email = sharedPreferences.getString(emailKey, "");
+            String password = sharedPreferences.getString(passwordKey, "");
+
+            goToHomeActivity(email, password);
+        }
+        else {
+            setContentView(R.layout.activity_login);
+        }
+        // setContentView(R.layout.activity_login);
     }
 
     public void toRegister(View view) {
@@ -19,8 +37,27 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void toHome(View view) {
+    public void goToHomeActivity(String email, String password) {
         Intent intent = new Intent(this, HomeActivity.class);
+
+        intent.putExtra("EMAIL", email);
+        intent.putExtra("PASSWORD", password);
+
         startActivity(intent);
+    }
+
+    public void toHome(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.sayyes", Context.MODE_PRIVATE);
+
+        EditText email = (EditText) findViewById(R.id.email);
+        String emailStr = email.getText().toString();
+
+        EditText password = (EditText) findViewById(R.id.password);
+        String passwordStr = password.getText().toString();
+
+        sharedPreferences.edit().putString("email", emailStr).apply();
+        sharedPreferences.edit().putString("password", passwordStr).apply();
+
+        goToHomeActivity(emailStr, passwordStr);
     }
 }
--- app/src/main/java/com/example/sayyes/MainActivity.java
+++ app/src/main/java/com/example/sayyes/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -23,7 +26,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        String emailKey = "email";
+        String passwordKey = "password";
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.sayyes", Context.MODE_PRIVATE);
+
+        if (!sharedPreferences.getString(emailKey, "").equals("") && !sharedPreferences.getString(passwordKey, "").equals("")){
+            String email = sharedPreferences.getString(emailKey, "");
+            String password = sharedPreferences.getString(passwordKey, "");
+
+            goToHomeActivity(email, password);
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    public void toHome(){
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.sayyes", Context.MODE_PRIVATE);
+
+        EditText email = (EditText) findViewById(R.id.email);
+        String emailStr = email.getText().toString();
+
+        EditText password = (EditText) findViewById(R.id.password);
+        String passwordStr = password.getText().toString();
+
+        sharedPreferences.edit().putString("email", emailStr).apply();
+        sharedPreferences.edit().putString("password", passwordStr).apply();
+
+        goToHomeActivity(emailStr, passwordStr);
+    }
+
+    public void goToHomeActivity(String email, String password) {
+        Intent intent = new Intent(this, HomeActivity.class);
+
+        intent.putExtra("EMAIL", email);
+        intent.putExtra("PASSWORD", password);
+
+        startActivity(intent);
     }
 
     public void toLogin(View view){
@@ -31,8 +72,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void toHome(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-    }
+
 }
