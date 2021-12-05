f92932d85442a9734a313b454548e79577063f32
whitney.huang <whuang258@wisc.edu>
2021-12-05 15:50:36 -0600

    Set name in Profile fragment

--- app/src/main/java/com/example/sayyes/ProfileFragment.java
+++ app/src/main/java/com/example/sayyes/ProfileFragment.java
@@ -1,12 +1,27 @@
 package com.example.sayyes;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    View inflatedView = null;
+    String userID;
+
+    // firebase authentication
+    FirebaseAuth fAuth;
+
+    // firebase user information storage
+    FirebaseFirestore fStore;
 
     public ProfileFragment(){
 
@@ -28,7 +52,38 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        inflatedView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // firebase authentication
+        fAuth = FirebaseAuth.getInstance();
+
+        // firebase storage
+        fStore = FirebaseFirestore.getInstance();
+
+        setUserProfile();
+//        TextView name = (TextView) inflatedView.findViewById(R.id.Name);
+//        name.setText("hello");
+
+        return inflatedView;
+    }
+
+    public void setUserProfile(){
+        TextView name = (TextView) inflatedView.findViewById(R.id.Name);
+        TextView id = (TextView) inflatedView.findViewById(R.id.ID);
+
+        // get current userID from authentication
+        userID = fAuth.getCurrentUser().getUid();
+
+        // fetch user data from storage using userID
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                Log.d(TAG, "set profile");
+                name.setText(documentSnapshot.getString("name"));
+                //id.setText(userID);
+            }
+        });
     }
 
 }
