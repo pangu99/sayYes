07034aaa97df77a79dfa27d0af771c29e9bf40f6
YukeWu <wuyuke0406@gmail.com>
2021-12-11 22:00:37 -0600

    Rewrote home with RecyclerView.

--- app/src/main/java/com/example/sayyes/HomeFragment.java
+++ app/src/main/java/com/example/sayyes/HomeFragment.java
@@ -9,11 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
     public HomeFragment() {
@@ -22,7 +18,9 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
--- app/src/main/java/com/example/sayyes/PostAdapter.java
+++ app/src/main/java/com/example/sayyes/PostAdapter.java
@@ -1,14 +1,62 @@
-//package com.example.sayyes;
-//
-//import android.widget.ImageView;
-//
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//public class PostAdapter {
-//
-//    public class ViewHolder extends RecyclerView.ViewHolder{
-//        public ImageView image_profile, post;
-//    }
-//
-//
-//}
+package com.example.sayyes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+
+    public Context mContext;
+    public List<Post> mPost;
+
+    private FirebaseUser firebaseUser;
+
+    public PostAdapter(Context mContext, List<Post> mPost){
+        this.mContext = mContext;
+        this.mPost = mPost;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.single_post, viewGroup, false);
+        return new PostAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Post post = mPost.get(i);
+        Glide.with(mContext).load(post.getP)
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView post_image;
+        public TextView post_title;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            post_image = itemView.findViewById(R.id.post_image);
+            post_title = itemView.findViewById(R.id.post_title);
+        }
+    }
+
+
+}
